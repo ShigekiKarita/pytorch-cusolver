5567b4f6d58cc2baa630a7ff72c5c3414ceb3f8e
karita <shigekikarita@gmail.com>
2018-10-05 22:37:04 +0900

    update readme

--- test.py
+++ test.py
@@ -1,19 +1,18 @@
 # coding: utf-8
+import numpy
 import torch
 import torch_cusolver
 
 A = torch.rand(2, 3, 3).cuda()
 A = A.transpose(1, 2).matmul(A)
-print(A)
 w, V = torch_cusolver.cusolver_batch_eigh(A,
                                           False,
                                           True,
                                           1e-7,
                                           100,
                                           False)
-print(w, V)
-for i in range(A.shape[0]):
-    print(i)
-    print(A[i])
-    print(V[i].t().matmul(w[i].diag()).matmul(V[i]))
 
+for i in range(A.shape[0]):
+    a = A[i]
+    e = V[i].t().matmul(w[i].diag()).matmul(V[i])
+    torch.testing.assert_allclose(a, e)
